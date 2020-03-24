a59502418ee92bd7bc90ed6ac947c618f7e8417d
SHAIK SADDAM <shksaddam@gmail.com>
2020-03-25 00:48:20 +0530

    initial commi

---
+++ Amazon/Amazon.sh
@@ -0,0 +1,6 @@
+hello guys 
+we are goinf to create new file with the name of AMAZON.sh 
+we need to look out the hoe to create file and repesitory and all 
+upadet codes what we need 
+Thank You Guys . Hope we will learn git hub very well . 
+Thank You , See you Soon With new coding . 
